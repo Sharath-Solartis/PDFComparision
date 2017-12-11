59fa3a67f1a71271852aa68dc13472e1da6f2c34
Sharath-Solartis <sharath_n@solartis.net>
2017-12-11 14:02:11 +0530

    comparison

--- src/main/java/PDF/PDFComparison.java
+++ src/main/java/PDF/PDFComparison.java
@@ -1,11 +1,19 @@
 package PDF;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLException;
 
 import de.redsix.pdfcompare.PdfComparator;
 
 public class PDFComparison 
 {
+	public static ExcelOperationsJXL objectLoginScript=null;
 	public void pdfcompare(String PDFpath1, String PDFpath2, String ResultPDFPath) throws IOException
 	{
 		
@@ -19,6 +27,14 @@ public class PDFComparison
             System.out.println("Difference not found in PDFs");
         }
         System.out.println("PDF ProcessCompleted");
+	}
+	
+	public static void urltopdf(String URL,String path,String filename) throws IOException
+	{
+		URL website = new URL(URL);
+		Path targetPath = new File(path + File.separator + filename+".pdf").toPath();
+		InputStream in = website.openStream();		
+		Files.copy(in, targetPath, StandardCopyOption.REPLACE_EXISTING);		
 	}
 	
 	/*public void pdfcompare1() throws IOException
@@ -44,13 +60,34 @@ public class PDFComparison
 		pdfUtil.compare(pdfpath1, pdfpath2);
 	}*/
 	
-	public static void main(String args[]) throws IOException
+	public static void main(String args[]) throws IOException, ClassNotFoundException, SQLException
 	{
-		String file1="Q:/Manual Testing/Starr/Starr-GL/FormsTemplate/All Forms/SIIL DS 01 (0117).pdf";
-        String file2="Q:/Manual Testing/Starr/Starr-GL/FormsTemplate/All Forms/SIIL DS 01 FL (0117) Common Policy Dec Page.pdf";
-        String result="D:/ftl/resultPDF";
-		PDFComparison newcompare = new PDFComparison();
-		newcompare.pdfcompare(file1,file2,result);
+		//propertiesHandle configFile = new propertiesHandle("A:/1 Projects/20 CHIC_UI/Release1/Configuration/Config_C1128.properties");
+		//databaseOperartions objectInput = new databaseOperartions();
+		//databaseOperartions.conn_setup(configFile);
+		//System.setProperty("jsse.enableSNIExtension", "false");	
+		//objectInput.get_dataobjects(configFile.getProperty("pdfexcelfile"));
+		//int i=1;
+		
+			for(int i=3;i<=58;i++)
+			{
+			//String QAPDFNAME=objectInput.read_data("pdf1");
+			//String POSTPRODPDFNAME =objectInput.read_data("pdf2");
+			//System.out.println(QAPDFNAME+POSTPRODPDFNAME);
+			String QApath="A:\\1 Projects\\09 ISO\\PDFCompareResults\\qa\\TestData"+i+".pdf";
+			String Mockpath="A:\\1 Projects\\09 ISO\\PDFCompareResults\\mock\\TestData"+i+".pdf";
+			//urltopdf(QAPDFNAME,"A:/1 Projects/09 ISO/PDFCompareResults/qa/","TestData"+objectInput.read_data("S.No"));
+			//urltopdf(QAPDFNAME,"A:/1 Projects/09 ISO/PDFCompareResults/mock/","TestData"+objectInput.read_data("S.No"));
+			PDFComparison newcompare = new PDFComparison();
+			System.out.println(QApath);
+			System.out.println(Mockpath);
+			newcompare.pdfcompare(QApath,Mockpath,"A:/1 Projects/09 ISO/PDFCompareResults/"+"TestDataCompareResult"+i);
+			}
+		//String file1="Q:/Manual Testing/Starr/Starr-GL/FormsTemplate/All Forms/SIIL DS 01 (0117).pdf";
+       // String file2="Q:/Manual Testing/Starr/Starr-GL/FormsTemplate/All Forms/SIIL DS 01 FL (0117) Common Policy Dec Page.pdf";
+        //String result="D:/ftl/resultPDF";
+		//PDFComparison newcompare = new PDFComparison();
+		//newcompare.pdfcompare(file1,file2,result);
 		//newcompare.pdfcompare1();
 	}
 }
